718e9fb48480e64edb40d87508f6196aea1b827c
Sharuf shaik <shaiksharuf95@gmail.com>
2023-03-17 18:45:22 +0530

    functions added

--- 2.Javascript-Functions/code/3.higher-order-functions.js
+++ 2.Javascript-Functions/code/3.higher-order-functions.js
@@ -3,7 +3,6 @@ var xyz = 100
 function printnumber (placeholder){
 console.log(placeholder)
 };
-
 printnumber(xyz)    // 100
 printnumber("xyz") // xyz Undefined
 
@@ -12,16 +11,13 @@ printnumber("xyz") // xyz Undefined
 
 function add(a,b){
 return a+b;
-}
-                  
+}                 
 function subract(a,b){
 return a-b;
-}
-                  
+}                 
 function multiply(a,b){
 return a*b;
-}
-                  
+}                 
 function division(a,b){
 return a/b;
 }
@@ -48,13 +44,36 @@ console.log(Div)
 
 
 
+const adding = "+"
+const subractt = "-"
+function calc(a,b,operation) {
+return operation(a,b)
+}
+function add (a,b){
+return a+b;
+}
+function subract(a,b){
+return a-b;
+}
+const result1 = calc(10,2,add);
+const result2 = calc(20,2,subract)
+console.log(result1);
+console.log(result2)
+
+
+
+
+
+
+
+
+
 
 const add = "+";
 const subract = "-";
 const multiply = "*";
 const division = "/";
                   
-
 calc();
 
 function calc(a,b,operator){
@@ -84,9 +103,57 @@ return total;
 
 
 
+function returndata(a,b,c) {
+                    console.log(`a = ${a}`)
+                    console.log(`b = ${b}`)
+                    console.log(`c = ${c}`)
+                    
+                    if (!c){
+                    return "ab"
+                    }
+                    if (!b){
+                    return "ac"
+                    }
+                    if (!a){
+                    return "ba"
+                    }
+                     return {
+                         a,
+                         b,
+                         c               
+                     }
+                    
+                    }
+                    returndata();
+                    console.log(returndata("shaik","sharuf","zakeer"));
+                    
 
 
 
 
 
 
+
+
+
+function returndata(person1,person2,person3){
+console.log(`person1 = ${person1}`)
+console.log(`person2 = ${person2}`)
+console.log(`person3 = ${person3}`) 
+                     
+if (!person1){
+return "Go to home"
+}
+if (!person2){
+return "Go to movie"
+}
+return {
+person1,
+person2,
+person3
+}
+}  
+                     
+
+returndata("shaik","Sharuf","zakeer");
+console.log(returndata("shaik","Sharuf","zakeer"));
